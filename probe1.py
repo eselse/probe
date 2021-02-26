889d6208b04060f78c399576f92c067233522ef9
eselse <ilikewebdevelopment@gmail.com>
2021-02-26 12:46:27 +0400

    Write simple script

--- probe1.py
+++ probe1.py
@@ -1,1 +1,8 @@
-print("Hello")
+"""
+This simple script shows numbers from 1 to 100
+from that we can extract quadratic root and then multiplied by itself
+"""
+for i in range(100):
+    number = i+1
+    if (number ** 0.5) * (number ** 0.5) == number:
+        print(number)
